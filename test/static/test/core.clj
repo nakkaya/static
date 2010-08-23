8ca46ee28ed502ded7128c7ad522518305b7c5db
Nurullah Akkaya <nurullah@nakkaya.com>
2010-08-23 21:08:09 +0300

    add tests

--- test/static/test/core.clj
+++ test/static/test/core.clj
@@ -1,6 +1,33 @@
 (ns static.test.core
   (:use [static.core] :reload)
-  (:use [clojure.test]))
+  (:use [static.markdown] :reload)
+  (:use [static.test.dummy-fs] :reload)
+  (:use [clojure.test])
+  (:use [clojure.contrib.io :only [delete-file-recursively]])
+  (:import (java.io File)))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defn dummy-fs-fixture [f]
+  (create-dummy-fs)
+  (create "resources/" "html/" "UTF-8")
+  (f)
+  (delete-file-recursively (File. "resources/") true)
+  (delete-file-recursively (File. "html/") true))
+
+(use-fixtures :once dummy-fs-fixture)
+
+(deftest test-markdown
+  (let [[metadata content] (read-markdown "resources/site/dummy.markdown")] 
+    (is (= "unit test"  (:tags metadata)))
+    (is (= "some dummy desc" (:description metadata)))
+    (is (= "dummy content" (:title metadata)))
+    (is (= "Some dummy file for unit testing."
+	   (re-find #"Some dummy file for unit testing." content)))))
+
+(deftest test-process-site
+  (let [html (File. "html/dummy.html")
+	static (File. "html/dummy.static")] 
+    (is (= true (.exists html)))
+    (is (= true (.exists static)))
+    (is (= "Some dummy file for unit testing."
+	   (re-find #"Some dummy file for unit testing." (slurp html))))
+    (is (= "Hello, World!!" (re-find #"Hello, World!!" (slurp static))))))
---
+++ test/static/test/dummy_fs.clj
@@ -0,0 +1,32 @@
+(ns static.test.dummy-fs
+  (:use [clojure.contrib.io :only [delete-file-recursively]])
+  (:import (java.io File)))
+
+(defn- create-resources []
+  (.mkdir (File. "resources/"))
+  (.mkdir (File. "resources/site/"))
+  (.mkdir (File. "resources/public/"))
+  (.mkdir (File. "resources/templates/")))
+
+(defn- create-site []
+  (spit (File. "resources/site/dummy.markdown")
+    "---
+title: dummy content
+description: some dummy desc
+tags: unit test
+template: temp.clj
+---
+
+Some dummy file for unit testing."))
+
+(defn- create-template []
+  (spit (File. "resources/templates/temp.clj") " $content$ "))
+
+(defn- create-static-file []
+  (spit (File. "resources/public/dummy.static") "Hello, World!!"))
+
+(defn create-dummy-fs []
+  (create-resources)
+  (create-site)
+  (create-static-file)
+  (create-template))

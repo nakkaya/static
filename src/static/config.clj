10c83b312552cb7cab2a8c86218d5df5e59a0a68
Nurullah Akkaya <nurullah@nakkaya.com>
2011-12-10 21:30:56 +0200

    set configuration defaults

--- src/static/config.clj
+++ src/static/config.clj
@@ -2,21 +2,30 @@
   (:use [clojure.contrib.logging])
   (:import (java.io File)))
 
-(def config
-     (memoize
-      #(try 
-	 (let [config (apply hash-map (read-string (slurp (File. "config.clj"))))]
-	   ;;if emacs key is set make sure executable exists.
-	   (when (:emacs config)
-	     (if (not (.exists (File. (:emacs config))))
-	       (do (error "Path to Emacs not valid.")
-		   (System/exit 0))))
-	   config)
+
+(let [defaults {:site-title "A Static Blog"
+                :site-description "Default blog description"
+                :site-url "https://github.com/nakkaya/static"
+                :in-dir "resources/"
+                :out-dir "html/"
+                :default-template "default.clj"
+                :encoding "UTF-8"
+                :posts-per-page 2
+                :blog-as-index true}]
+  
+  (def config
+    (memoize
+     #(try 
+        (let [config (apply hash-map (read-string (slurp (File. "config.clj"))))]
+          ;;if emacs key is set make sure executable exists.
+          (when (:emacs config)
+            (if (not (.exists (File. (:emacs config))))
+              (do (error "Path to Emacs not valid.")
+                  (System/exit 0))))
+          (merge defaults config))
 	(catch Exception e (do 
 			     (info "Configuration not found using defaults.")
-			     {:in-dir "resources/"
-			      :out-dir "html/"
-			      :encoding "UTF-8"})))))
+			     defaults))))))
 
 (defn set!-config [k v]
   (alter-var-root (find-var 'static.config/config) (fn [c] #(identity (assoc (c) k v)))))

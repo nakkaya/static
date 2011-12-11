70deb87d95d17b4f01409cbd69aee0b5f3c204a3
Nurullah Akkaya <nurullah@nakkaya.com>
2011-12-11 17:01:30 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject static "1.0.0-SNAPSHOT"
+(defproject static "1.1.0-SNAPSHOT"
   :description "Simple static site generator."
   :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.clojure/clojure-contrib "1.2.0"]

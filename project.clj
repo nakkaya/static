4f7e22facf3577061123b68c43eea78f0c0f6e45
Clinton R. Nixon <crnixon@gmail.com>
2012-03-15 22:17:13 -0400

    Changed Markdown processor to pegdown
    - org.pegdown.PegDownProcessor is a more recently maintained Markdown
      library and has options to handle more of the extended Markdown
      syntax people have grown to love

--- project.clj
+++ project.clj
@@ -4,10 +4,11 @@
                  [org.clojure/tools.cli "0.2.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [hiccup "0.3.8"]
-                 [org.markdownj/markdownj "0.3.0-1.0.2b4"]
+                 [org.pegdown/pegdown "1.1.0"]
                  [org.clojars.amit/commons-io "1.4.0"]
                  [ring/ring-core "1.0.2"]
                  [ring/ring-jetty-adapter "1.0.2"]]
+  
   :main static.core
   :jar-name "static.jar"
   :uberjar-name "static-app.jar")

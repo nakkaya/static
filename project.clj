5d41fa6e569f78f6a4e2a93764d1221dfb100ec0
Clinton R. Nixon <crnixon@gmail.com>
2012-03-15 22:01:53 -0400

    Fixing indentation

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.clojure/tools.logging "0.2.3"]
-		 [hiccup "0.3.8"]
-		 [org.markdownj/markdownj "0.3.0-1.0.2b4"]
-		 [org.clojars.amit/commons-io "1.4.0"]
-		 [ring/ring-core "1.0.2"]
-		 [ring/ring-jetty-adapter "1.0.2"]]
+                 [hiccup "0.3.8"]
+                 [org.markdownj/markdownj "0.3.0-1.0.2b4"]
+                 [org.clojars.amit/commons-io "1.4.0"]
+                 [ring/ring-core "1.0.2"]
+                 [ring/ring-jetty-adapter "1.0.2"]]
   :main static.core
   :jar-name "static.jar"
   :uberjar-name "static-app.jar")

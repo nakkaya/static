5eba24a66e0b58e523a477e6ff01a580cafe8076
Nurullah Akkaya <nurullah@nakkaya.com>
2012-07-25 02:11:51 +0300

    Revert "wrap slurped css file in a vector"
    This reverts commit 54825e15f1b4fc3d5b919179c99b95d485a341bb.

--- src/static/io.clj
+++ src/static/io.clj
@@ -55,7 +55,7 @@
 (defn- read-cssgen [file]
   (let [metadata {:extension "css" :template :none}
         content (read-string
-                 (str \[ (slurp file :encoding (:encoding (config))) \]))
+                 (slurp file :encoding (:encoding (config))))
         to-css  #(clojure.string/join "\n" (doall (map css %)))]
     [metadata (delay (binding [*ns* (the-ns 'static.core)] (-> content eval to-css)))]))
 

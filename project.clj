bab5be5e54547dacbdbcb5d80702e3e4a416916d
Cesar Canassa <cesar.canassa@gmail.com>
2013-10-06 01:03:36 -0300

    Updated all dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject static "1.1.0-SNAPSHOT"
   :description "Simple static site generator."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.cli "0.2.1"]
-                 [org.clojure/tools.logging "0.2.3"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [org.clojure/tools.logging "0.2.6"]
                  [cssgen "0.3.0-SNAPSHOT"]
-                 [hiccup "0.3.8"]
-                 [org.pegdown/pegdown "1.2.1"]
+                 [hiccup "1.0.4"]
+                 [org.pegdown/pegdown "1.4.1"]
                  [org.clojars.amit/commons-io "1.4.0"]
-                 [ring/ring-core "1.0.2"]
-                 [ring/ring-jetty-adapter "1.0.2"]
-                 [watchtower "0.1.0"]
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-jetty-adapter "1.2.0"]
+                 [watchtower "0.1.1"]
                  [stringtemplate-clj "0.1.0"]]
   :main static.core
   :jar-name "static.jar"
